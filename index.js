431d577a2152c4e686d5976d88ee9a1f10b9c298
Aeriel Waters <aeriel.waters@gmail.com>
2019-10-17 22:26:16 -0500

    update final fix for add section.

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 /* 
-1. Enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button.
 2. check and uncheck items on the list by clicking the "Check" button
 3. permanently remove items from the list
-7. Using DOM manipulation and traversal to dynamically add and remove HTML elements and apply styles
-10. Be sure to put both script elements at the bottom of the <body> element.
 12. Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
 */
 
-$('.container').ready(function(){
-
     // Adding items to the list
     
-      // Define callback function
+    // Define callback function
       const addItem = function() {
         // Declare variable to capture input text value
         const text = $('#shopping-list-entry').val();
+        console.log('text', text);
         // If the input text field isn't empty, add it to the list as a new item
         if (text) {
-          $('#shopping-list-entry').prepend('<li class="shopping-item"><a class="shopping-item-delete">delete</a>' + text + '<a class="shopping-item-toggle">check</a></li>');
+          $('.shopping-list').prepend(`
+          <li>
+            <span class="shopping-item">${text}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+                </button>
+             <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+             </button>
+            </div>
+          </li>`);
         }
         // Clear input text field
-        $('.submit').val("");
+        $('#shopping-list-entry').val("");
       };
     
       // Add new item to list by clicking "Add" button
-      $('.submit').on('click', function(event){
+      $('#js-shopping-list-form').submit( function(event){
         // (prevents form submit button unwanted default action)
         event.preventDefault();
         // run callback function
         addItem();
-      });
-    
-      // Add new item to the list by hitting "Enter"
-      $('.submit').keypress(function(event){
-        if (event.which === 13) {
-          // run callback function
-          addItem();
-        }
       });
     
     // Deleting items from list
@@ -66,4 +65,4 @@ $('.container').ready(function(){
           $('.shopping-list').prepend($listItem);
         }
       });
-    });
+   // });
