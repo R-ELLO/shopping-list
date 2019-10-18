2785e6926d362a1e79aceb45f3c8cf3293f9b587
Aeriel Waters <aeriel.waters@gmail.com>
2019-10-17 20:28:52 -0500

    JS and HTML updates

--- index.js
+++ index.js
@@ -4,63 +4,61 @@
 3. permanently remove items from the list
 7. Using DOM manipulation and traversal to dynamically add and remove HTML elements and apply styles
 10. Be sure to put both script elements at the bottom of the <body> element.
-11. Do not alter index.html or main.css other than adding the links to the external JavaScript inside index.html. Write JavaScript code that works with the existing HTML and CSS to implement the required features.
 12. Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
 */
 
-$(document).ready(function(){
+$('container').ready(function(){
 
-    // ADDING A NEW ITEM TO THE LIST
+    // Adding items to the list
     
-      // Define the callback function
+      // Define callback function
       const addItem = function() {
-    
-        // Declare a variable to capture the input text value
-        const $input = $('#shopping-list-entry').val();
+        // Declare variable to capture input text value
+        const text = $('shopping-list-entry').val();
         // If the input text field isn't empty, add it to the list as a new item
-        if ($input) {
-          $('.shopping-list').prepend('<li class="shopping-item"><a class="shopping-item-delete">X</a>' + $input + '<a class="button-label">✔</a></li>');
+        if (text) {
+          $('shopping-list-entry').prepend('<li class="shopping-item"><a class="shopping-item-delete">delete</a>' + text + '<a class="shopping-item-toggle">check</a></li>');
         }
-        // Clear the input text field
-        $('.submit').val("");
+        // Clear input text field
+        $('submit').val("");
       };
     
-      // Add a new item to the list by clicking "Add" button
-      $('.submit').on('click', function(event){
+      // Add new item to list by clicking "Add" button
+      $('submit').on('click', function(event){
         // (prevents form submit button unwanted default action)
         event.preventDefault();
-        // run the callback function
+        // run callback function
         addItem();
       });
     
-      // Add a new item to the list by hitting "Enter"
-      $('.submit').keypress(function(event){
+      // Add new item to the list by hitting "Enter"
+      $('submit').keypress(function(event){
         if (event.which === 13) {
-          // run the callback function
+          // run callback function
           addItem();
         }
       });
     
-    // DELETING AN ITEM FROM THE LIST
+    // Deleting items from list
     
-      // Clicking an item's delete button:
+      // Clicking item's delete button:
       $('.shopping-list').on('click', '.shopping-item-delete', function(){
-        // removes that item from the list
+        // removes clicked item from the list
         $(this).parent().fadeOut(300, function(){
           $(this).remove();
         });
       });
     
-    // CHECKING AN ITEM OFF FROM THE LIST
+    // Checking item off list
     
-      // Clicking an item's check button:
+      // Clicking item's check button:
       $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
         // grays everything out
         $(this).parent().toggleClass('.shopping-item__checked');
         $(this).siblings().toggleClass('.shopping-item-delete');
         $(this).toggleClass('.shopping-item__checked');
     
-        // moves the element to either the bottom or top of the list
+        // moves element to either the bottom or top of the list
         var $listItem = $(this).parent();
         if ($listItem.hasClass('.shopping-item__checked')) {
           $('.shopping-list').append($listItem);
@@ -68,13 +66,4 @@ $(document).ready(function(){
           $('.shopping-list').prepend($listItem);
         }
       });
-    
-    // MAKING LIST ITEMS SORTABLE
-    
-      $('.shopping-list').sortable({
-          distance: 2,
-          revert: 300,
-          cancel: ".shopping-item__checked"
-      });
-    
     });
